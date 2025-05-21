1aa7f8038db3b1c251b3a65033eb6b31b90373f9
LYChubby <f4uzi345@gmail.com>
2025-05-21 21:07:56 +0700

    Membuat Appbar

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:plantsapp/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: buildAppBar(), body: Body());
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+    );
+  }
+}
