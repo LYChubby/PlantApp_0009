1ae6cb0aae4c7419de24d592c4782180d765646f
LYChubby <f4uzi345@gmail.com>
2025-05-21 22:02:56 +0700

    Memindahkan Code Tadi Ke Dalam File Header

---
+++ lib/screens/home/components/header_with_searchbox.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:plantsapp/constanst.dart';
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({super.key, required this.size});
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: size.height * 0.2 - 27,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  "Hi Fauzi !",
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Spacer(),
+                CircleAvatar(
+                  radius: 50, // Ukuran radius
+                  backgroundImage: AssetImage("assets/images/profile.jpeg"),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              height: 54,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: TextField(
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  hintStyle: TextStyle(color: kPrimaryColor.withOpacity(0.5)),
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  suffixIcon: Icon(Icons.search, color: kPrimaryColor),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
