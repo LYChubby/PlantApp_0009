8fc23433d46ee9fd100a88a2b22db46a21149437
LYChubby <f4uzi345@gmail.com>
2025-05-21 23:40:32 +0700

    Membuat Fungsi Untuk Recomends Plants

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:plantsapp/constanst.dart';
 import 'package:plantsapp/screens/home/components/header_with_searchbox.dart';
+import 'package:plantsapp/screens/home/components/recomends_plants.dart';
 import 'package:plantsapp/screens/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
@@ -14,114 +15,7 @@ class Body extends StatelessWidget {
         children: <Widget>[
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: "Recommended", press: () {}),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                RecomendPlantCard(
-                  image: "assets/images/image_1.png",
-                  title: "Samantha",
-                  country: "Russia",
-                  price: 440,
-                  press: () {},
-                ),
-                RecomendPlantCard(
-                  image: "assets/images/image_2.png",
-                  title: "Angelica",
-                  country: "Russia",
-                  price: 440,
-                  press: () {},
-                ),
-                RecomendPlantCard(
-                  image: "assets/images/image_3.png",
-                  title: "Samantha",
-                  country: "Russia",
-                  price: 440,
-                  press: () {},
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class RecomendPlantCard extends StatelessWidget {
-  const RecomendPlantCard({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.country,
-    required this.price,
-    required this.press,
-  });
-
-  final String image, title, country;
-  final int price;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.only(
-        left: kDefaultPadding,
-        top: kDefaultPadding / 2,
-        bottom: kDefaultPadding * 2.5,
-      ),
-      width: size.width * 0.4,
-      child: Column(
-        children: <Widget>[
-          Image.asset(image),
-          GestureDetector(
-            onTap: press,
-            child: Container(
-              padding: EdgeInsets.all(kDefaultPadding / 2),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(0.23),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "$title\n".toUpperCase(),
-                          style: Theme.of(context).textTheme.labelLarge,
-                        ),
-                        TextSpan(
-                          text: "$country".toUpperCase(),
-                          style: TextStyle(
-                            color: kPrimaryColor.withOpacity(0.5),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    '\$$price',
-                    style: Theme.of(
-                      context,
-                    ).textTheme.labelLarge?.copyWith(color: kPrimaryColor),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          RecomendsPlants(),
         ],
       ),
     );
