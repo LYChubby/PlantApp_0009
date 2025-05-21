6825a156dc1637012a556a7de153c3a9a1279ee3
LYChubby <f4uzi345@gmail.com>
2025-05-21 22:46:39 +0700

    Membuat Title With More

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:plantsapp/constanst.dart';
+import 'package:plantsapp/screens/home/components/header_with_searchbox.dart';
+import 'package:plantsapp/screens/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -8,85 +10,10 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: Column(children: <Widget>[HeaderWithSearchBox(size: size)]),
-    );
-  }
-}
-
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({super.key, required this.size});
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
-      height: size.height * 0.2,
-      child: Stack(
+      child: Column(
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(
-              left: kDefaultPadding,
-              right: kDefaultPadding,
-              bottom: 36 + kDefaultPadding,
-            ),
-            height: size.height * 0.2 - 27,
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(36),
-                bottomRight: Radius.circular(36),
-              ),
-            ),
-            child: Row(
-              children: <Widget>[
-                Text(
-                  "Hi Fauzi !",
-                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Spacer(),
-                CircleAvatar(
-                  radius: 50, // Ukuran radius
-                  backgroundImage: AssetImage("assets/images/profile.jpeg"),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 54,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(0.23),
-                  ),
-                ],
-              ),
-              child: TextField(
-                textAlignVertical: TextAlignVertical.center,
-                decoration: InputDecoration(
-                  hintText: "Search",
-                  hintStyle: TextStyle(color: kPrimaryColor.withOpacity(0.5)),
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  suffixIcon: Icon(Icons.search, color: kPrimaryColor),
-                ),
-              ),
-            ),
-          ),
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: "Recommended", press: () {}),
         ],
       ),
     );
